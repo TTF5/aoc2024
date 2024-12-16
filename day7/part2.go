05238531e636c155f2c4265803a1e661ab65e89e
Jonathan König <koenig-jonathan@web.de>
2024-12-16 22:50:25 +0100

    Parallelize Day7

--- day7/part2.go
+++ day7/part2.go
@@ -2,7 +2,10 @@ package day7
 
 import (
 	"fmt"
+	"math/big"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/TTF5/AoC2024/utility"
 )
@@ -24,23 +27,49 @@ func checkEquation2(eq Equation, ops []Operation) bool {
 	return result == eq.result
 }
 
+func worker(results chan<- int, eq Equation) {
+	fmt.Println("Processing", eq)
+	ch := utility.GenerateCombinations([]Operation{OP_ADD, OP_CONCAT, OP_MULT}, len(eq.values)-1)
+	for ops := range ch {
+		if checkEquation2(eq, ops) {
+			results <- eq.result
+			break
+		}
+	}
+}
+
 func Day7Part2() {
 	eqns := parseInput("day7/input.txt")
 
-	calibResult := 0
-
+	results := make(chan int, len(eqns))
+	var wg sync.WaitGroup
 	noEqns := len(eqns)
 
-	for i, eq := range eqns {
-		ch := utility.GenerateCombinations([]Operation{OP_ADD, OP_CONCAT, OP_MULT}, len(eq.values)-1)
-		fmt.Println("Checking equation", i+1, "of", noEqns)
-		for ops := range ch {
-			if checkEquation2(eq, ops) {
-				calibResult += eq.result
-				break
-			}
-		}
+	start := time.Now()
+
+	wg.Add(noEqns)
+	for _, eq := range eqns {
+		go func(eq Equation) {
+			worker(results, eq)
+			wg.Done()
+		}(eq)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	calibResult := 0
+	for val := range results {
+		calibResult += val
 	}
 
 	fmt.Println(calibResult)
+
+	r := new(big.Int)
+	fmt.Println(r.Binomial(1000, 10))
+
+	elapsed := time.Since(start)
+	fmt.Printf("Binomial took %s", elapsed)
 }
